88aac5045b67c519ac3f7c7ed0918ebf6bf4b655
Alberto Jose Marroquin Mendoza <54414927+00117719@users.noreply.github.com>
2019-09-23 22:38:35 -0600

    Add files via upload

---
+++ busquedabinaria1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int arr[]= {1,3,4,5,17,18,31,33};
+bool bBinaria( int low[], int high[], int x ){
+
+  low[]={1,3,4};
+ high[]={18,31,33};
+ cout <<"Digite un numero entero positivo"<<endl;
+    cin >> x;
+    
+	
+		for(int i=0; i<=arr; i++){
+			if(low%8==0){			
+			cout<<"Su elemento se encuentra en:";
+			return  true;
+		}
+		else
+		{
+			cout<<"Su elemento se encuentra en:";
+			return  cout<<high<< true;
+		}
+		
+		}
+	}
+
+int main() {
+
+int x=0;
+cin>>x>>endl;
+
+bBinaria(c,b)
+
+    return 0;
+}
+	
