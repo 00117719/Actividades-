b22f84345f7e91167d6b51fcf4a682f8ef9b8cc6
Alberto Jose Marroquin Mendoza <54414927+00117719@users.noreply.github.com>
2019-09-29 22:26:42 -0600

    Add files via upload

---
+++ pilavacia.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+struct Tpila{
+	char elements[50];
+	int top;
+};
+typedef struct Tpila Pila;
+
+void initialize(Pila*s){
+	s->top=-1;
+}
+bool empty(Pila*s){
+	return s->top=-1;
+}
+
+void push(Pila *s, float e){
+	if(s->top < 49){
+		(s->top)++;
+		s->elements[s->top] = e;
+	}
+}
+
+void pop(Pila *s, float *e){
+	if(s->top >= 0){
+		*e = s->elements[s->top];
+		(s->top)--;
+	}
+}
+	
+
+int main()
+{
+	Pila unaPila;
+	initialize(&unaPila);
+	
+	if(empty(&unaPila))
+		cout << "Esta vacia" << endl;
+	
+	float x = 5.6;
+	push(&unaPila, x);
+	
+	if(empty(&unaPila))
+		cout << "Esta vacia" << endl;
+	
+	float y = 0;
+	pop(&unaPila, &y);
+	cout << "y: " << y << endl;
+	
+	if(empty(&unaPila))
+		cout << "Esta vacia" << endl;
+	
+	push(&unaPila, 0.1);
+	push(&unaPila, 10);
+	push(&unaPila, 9.5);
+	
+	while(!empty(&unaPila)){
+	 pop(&unaPila, &y);
+		cout << "y: " << y << endl;
+	}
+}
+	
